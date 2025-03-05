7fc079114d5821d38a0f7f29186b17f18454ac30
Daianne Soares Silva <daiannesoaressilva1@gmail.com>
2025-03-05 11:52:55 -0300

    funções atualizar lista e sorteado

--- app.js
+++ app.js
@@ -26,3 +26,30 @@ function limparCampo(){
     inputAmigo = document.querySelector('input');
     inputAmigo.value = '';
 }
+
+function atualizarLista(amigos){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""; //Limpa a lista antes de atualizar para garantir que não hajam duplicados.
+
+    for(let i = 0; i < amigos.length; i++){
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+    }
+    console.log(listaAmigos());
+
+    return listaAmigos;
+}
+
+function sortearAmigo(){
+    if (amigos.lenght == null){
+        alert('A lista de amigos está vazia');
+        return;
+    }
+
+    let sorteado = Math.floor(Math.random() * amigos.length);
+    let nomeSorteado = amigos[sorteado];
+
+    console.log(sorteado);
+    console.log(nomeSorteado);
+}
